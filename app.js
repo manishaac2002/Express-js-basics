2eb3e1e5c0d16456673a4a4068d1fa2bec7de3e0
manishaac2002 <manishaac2002@gmail.com>
2023-07-19 16:31:11 +0530

    learning feat -handling diffrent url/routers.

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const express =require('express')
 const application = express()
 
 //next - execute the next function after executing the current function
-application.use((request,response,next)=>{
-    console.log('First Middleware');
-    next()
+application.use('/third',(request,response,next)=>{
+    console.log('third Middleware');
+    response.send('<h2>third page</h2>')
 })
-application.use((request,response,next)=>{
-    console.log('Second Middleware');
-    next()
-})
-application.use((request,response,next)=>{
+application.use('/second',(request,response,next)=>{
     console.log('Third Middleware');
     //sending response in express
     response.send("<h1>Hey I'm there</h1>")
+    response.send('<h2>second page</h2>')
+})
+application.use('/',(request,response,next)=>{
+    console.log('First Middleware');
+    response.send('<h2>First page</h2>')
 })
 
 
