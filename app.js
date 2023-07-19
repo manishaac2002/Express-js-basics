e6990f108a4f8285b12d67b9ead8c64ed4f40b8d
manishaac2002 <manishaac2002@gmail.com>
2023-07-19 18:04:16 +0530

    chore--

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express =require('express')
+const bodyParser=require('body-parser')
 
 //storing a function to a variable
 const application = express()
 
+application.use(bodyParser.urlencoded())
+//parsing data from request
+application.use('/add-product',(request,response,next)=>{
+    response.send('<form action="/store-product" method="POST"><h1>Add Product details</h1><input type="text" name ="title"><input type="submit" value="submit"></form>')
+})
+
+application.use('/store-product',(request,response,next)=>{
+    console.log(request.body);
+    response.send('<b>Product Submitted..!</b>')
+})
+
+
+
+
+
+
+
+
+
+//handling different request & routers
 //next - execute the next function after executing the current function
 application.use('/third',(request,response,next)=>{
     console.log('third Middleware');
@@ -14,11 +35,11 @@ application.use('/second',(request,response,next)=>{
     response.send("<h1>Hey I'm there</h1>")
     response.send('<h2>second page</h2>')
 })
+//this is a main route first runs in browser
 application.use('/',(request,response,next)=>{
     console.log('First Middleware');
     response.send('<h2>First page</h2>')
 })
-
 
 application.listen(7000)
 
