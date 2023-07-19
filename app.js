9255cb981e9302c318e4358b5116f7bd82df6109
manishaac2002 <manishaac2002@gmail.com>
2023-07-19 15:23:38 +0530

    learning feat - in express -->response ,middleware: using next function.

--- app.js
+++ app.js
@@ -21,8 +21,3 @@ application.use((request,response,next)=>{
 
 application.listen(7000)
 
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/manishaac2002/Express-js-basics.git
-// git push -u origin main
